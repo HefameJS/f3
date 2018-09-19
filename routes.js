3bc6eeca01ed7d18f1c86aa98ada8a42ed7b30e1
Abad Carrascosa <Alejandro_AC@hefame.es>
2018-09-19 16:10:16 +0200

    Añadidos middleware para control de errores EXPRESS
    ANTES de ejecutar el controlador, se comprueba que no haya errores JSON. 
    En caso de errores se devuelve un error 400, utilizando el formato de 
    respuesta FedicomError.
    DESPUES, se comprueba si algún controlador se ejecutó. En caso negativo 
    devuelve un error 404, utilizando el formato de respuesta FedicomError.

--- routes.js
+++ routes.js
@@ -1,10 +1,37 @@
 
 'use strict';
 
+const FedicomError = require('./model/FedicomError');
+
 module.exports = function(app) {
   var authenticate = require('./controllers/authenticate');
 
+  /* Middleware que se ejecuta antes de buscar la ruta correspondiente.
+   * Detecta errores comunes en las peticiones entrantes tales como:
+   *  - Errores en el parseo del JSON entrante.
+   */
+  app.use(function (error, req, res, next) {
+    if (error) {
+      var fedicomError = new FedicomError(error);
+      return res.status(fedicomError.statusCode).json(fedicomError.toJson());
+    }
+    else {
+      next();
+    }
+  });
+
+  /* RUTAS */
   app.route('/authenticate')
 	.get(authenticate.test);
+
+
+
+
+
+  /* Middleware que se ejecuta tras no haberse hecho matching con ninguna ruta. */
+  app.use(function(req, res, next) {
+    var fedicomError = new FedicomError('CORE-404', 'No existe el endpoint indicado.', 404);
+    res.status(fedicomError.statusCode).json(fedicomError.toJson());
+  });
 
 };
