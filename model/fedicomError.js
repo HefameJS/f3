f14c6543ddfe762b95e1a349971c7a773212eefc
Abad Carrascosa <Alejandro_AC@hefame.es>
2018-09-21 15:57:20 +0200

    Añadida function merge para unir varios FedicomError en uno

--- model/fedicomError.js
+++ model/fedicomError.js
@@ -5,7 +5,32 @@ class FedicomError {
   constructor(error, descripcion, httpCode) {
     this.itemList = [];
     this.httpCode = 500;
-    this.add(error, descripcion, httpCode);
+
+    if (error) {
+      this.add(error, descripcion, httpCode);
+    }
+  }
+
+  merge(otherError) {
+    if (otherError && otherError.constructor.name === 'FedicomError') {
+      this.httpCode = otherError.httpCode;
+      this.itemList = FedicomError.merge(this, otherError).itemList;
+    }
+
+    return this;
+  }
+
+  static merge(a, b) {
+    if (a && a.constructor.name === 'FedicomError' && b && b.constructor.name === 'FedicomError') {
+      b.itemList.forEach( (error) => { a.itemList.push(error); });
+      return a;
+    } else {
+      if (a && a.constructor.name === 'FedicomError') return a;
+      if (b && b.constructor.name === 'FedicomError') return b;
+      return new FedicomError();
+    }
+
+
   }
 
   add(error, descripcion, httpCode) {
@@ -14,7 +39,7 @@ class FedicomError {
     if (error && descripcion && typeof error === 'string' && typeof descripcion === 'string') {
       this.httpCode = (typeof statusCode === 'number') ?  statusCode : this.httpCode ;
       this.itemList.push( { codigo: error, descripcion: descripcion } );
-      return;
+      return this;
     }
 
     // Se llama utilizando un error devuelto por Express.
@@ -27,12 +52,12 @@ class FedicomError {
         console.error('ERROR EXPRESS NO CONTROLADO: ' + error.type);
         this.itemList.push( { codigo: 'CORE-001', descripcion: 'Error desconocido [' + error.type + ']' } );
       }
-      return;
+      return this;
     }
 
     // Si los parámetros de la llamada no son válidos
     this.itemList.push( { codigo: 'CORE-000', descripcion: 'Error desconocido' } );
-
+    return this;
   }
 
   send(expressRes) {
