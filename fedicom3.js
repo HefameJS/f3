4dfe7414acf3e3689221b1926d6a453e5e1b3cc0
Abad Carrascosa <Alejandro_AC@hefame.es>
2018-11-28 16:40:43 +0100

    Añadido soporte SSL
    Ahora la aplicación soporta el uso simultáneo de HTTP y HTTPS. También 
    se ha implementado la configuración del servicio HTTP(s) a través del 
    fichero de configuración.

--- fedicom3.js
+++ fedicom3.js
@@ -1,22 +1,35 @@
 require('./util/nativeExtensions');
 global.config = require('./config');
 
+console.log('Concentrador Fedicom 3 - v0.0.1');
 
-// Carga de modelos
-// require('./api/models/pedidosModel');
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+var httpConf = global.config.http;
+var httpsConf = global.config.https;
+
+httpsConf.ssl = {
+    key: fs.readFileSync(httpsConf.key),
+    cert: fs.readFileSync(httpsConf.cert),
+	 passphrase: httpsConf.passphrase
+};
+
 
 var app = require('express')();
 app.use(require('body-parser').json({extended: true}));
 app.use(require('morgan')('dev'));
 app.use(require('express-bearer-token')());
 
-var port = process.env.PORT || 50000;
-app.listen(port);
-
 // Carga de rutas
 var routes = require('./routes');
 routes(app);
 
+var server = http.createServer(app).listen(httpConf.port, function(){
+  console.log("Servidor HTTP a la escucha en el puerto " + httpConf.port);
+});
 
-console.log('Concentrador Fedicom 3 - v0.0.1');
-console.log('Escuchando en el puerto ' + port)
+var secureServer = https.createServer(httpsConf.ssl, app).listen(httpsConf.port, function(){
+  console.log("Servidor HTTPS a la escucha en el puerto " + httpsConf.port);
+});
