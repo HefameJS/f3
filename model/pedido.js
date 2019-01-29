eb8b52229b6fd4eb98c4ce3ba2bba617456c0355
Abad Carrascosa <Alejandro_AC@hefame.es>
2019-01-29 16:04:19 +0100

    El tipo de pedido ya no es obligatorio

--- model/pedido.js
+++ model/pedido.js
@@ -37,7 +37,7 @@ class Pedido {
 		// SANEADO OBLIGATORIO
 		var fedicomError = new FedicomError();
 		if (!json.codigoCliente) fedicomError.add('PED-ERR-002', 'El campo "codigoCliente" es obligatorio', 400);
-		if (!json.tipoPedido) fedicomError.add('PED-ERR-003', 'El campo "tipoPedido" es obligatorio', 400);
+		// if (!json.tipoPedido) fedicomError.add('PED-ERR-003', 'El campo "tipoPedido" es obligatorio', 400);
 		if (!json.lineas || json.lineas.length === 0) fedicomError.add('PED-ERR-004', 'El campo "lineas" no puede estar vacío', 400);
 		if (!json.numeroPedidoOrigen) fedicomError.add('PED-ERR-006', 'El campo "numeroPedidoOrigen" es obligatorio', 400);
 		if (fedicomError.hasError())	throw fedicomError;
