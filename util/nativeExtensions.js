a99152de7f6f52c8341e7b32e14d39d53aaff9ff
Abad Carrascosa <Alejandro_AC@hefame.es>
2019-01-29 13:53:45 +0100

    Añadidas funciones de formato de fechas Fedicom3
    Se añade librería 'dateformat'

--- util/nativeExtensions.js
+++ util/nativeExtensions.js
@@ -1,3 +1,26 @@
+var dateFormat = require('dateformat');
+
+// Date.timestamp() devuelve el timestamp actual
 if (!Date.timestamp) {
     Date.timestamp = function() { return new Date().getTime(); }
+}
+
+
+dateFormat.masks.fedicomDate = 'dd-mm-yyyy';
+dateFormat.masks.fedicomDateTime = 'dd-mm-yyyy HH:MM:ss';
+
+//
+if (!Date.fedicomDate) {
+	Date.fedicomDate = function (date) {
+		if (!date) date = new Date();
+		return dateFormat(date, "fedicomDate")
+	}
+}
+
+
+if (!Date.fedicomDateTime) {
+	Date.fedicomDateTime = function (date) {
+		if (!date) date = new Date();
+		return dateFormat(date, "fedicomDateTime")
+	}
 }
