b065e8db5b4731c3dde5b91a3f62b26ba4b85338
Abad Carrascosa <Alejandro_AC@hefame.es>
2019-08-02 14:39:12 +0200

    Bug que causaba que valores 0 dieran que el campo no existe

--- util/fieldChecker.js
+++ util/fieldChecker.js
@@ -4,7 +4,7 @@ const L = global.logger;
 
 module.exports =  {
 	checkExists: function(field, errorObject, errorCode, errorDesc ) {
-		if (!field) {
+		if (field === null || field === undefined) {
 			if (errorObject) errorObject.add(errorCode, errorDesc, 400);
 			return true;
 		}
@@ -12,27 +12,30 @@ module.exports =  {
 	},
 
 	checkExistsAndPositiveInteger: function (field, errorObject, errorCode, errorDesc ) {
-		if (!field) {
-			if (errorObject) errorObject.add(errorCode, errorDesc, 400);
-			return true;
-		} else {
+		if (field || field === 0) {
 			var asInt = Number(field);
 			if (!asInt || asInt <= 0 || asInt === Number.NaN || asInt === Number.NEGATIVE_INFINITY || asInt === Number.POSITIVE_INFINITY ) {
 				if (errorObject) errorObject.add(errorCode, errorDesc, 400);
 				return true;
 			}
+		} else {
+			if (errorObject) errorObject.add(errorCode, errorDesc, 400);
+			return true;
 		}
 		return false;
 	},
 
 
 	checkPositiveInteger: function (field, errorObject, errorCode, errorDesc ) {
-		if (field) {
+		if (field || field === 0) {
 			var asInt = Number(field);
 			if (!asInt || asInt <= 0 || asInt === Number.NaN || asInt === Number.NEGATIVE_INFINITY || asInt === Number.POSITIVE_INFINITY ) {
 				if (errorObject) errorObject.add(errorCode, errorDesc, 400);
 				return true;
 			}
+		} else {
+			if (errorObject) errorObject.add(errorCode, errorDesc, 400);
+			return true;
 		}
 		return false;
 	},
