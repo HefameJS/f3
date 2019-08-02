7b075c94d8156ce10f954637446910e01a7b2a2f
Abad Carrascosa <Alejandro_AC@hefame.es>
2019-08-02 14:50:25 +0200

    Ahora se permite que el "orden" sea 0

--- util/fieldChecker.js
+++ util/fieldChecker.js
@@ -37,6 +37,17 @@ module.exports =  {
 		return false;
 	},
 
+	checkGreaterThanZero: function (field, errorObject, errorCode, errorDesc ) {
+		if (field || field === 0) {
+			var asInt = Number(field);
+			if (!asInt || asInt < 0 || asInt === Number.NaN || asInt === Number.NEGATIVE_INFINITY || asInt === Number.POSITIVE_INFINITY ) {
+				if (errorObject) errorObject.add(errorCode, errorDesc, 400);
+				return true;
+			}
+		}
+		return false;
+	},
+
 	checkExistsAndNonEmptyArray: function checkExistsAndNonEmptyArray(field, errorObject, errorCode, errorDesc ) {
 		if (!field || !field.forEach || field.length < 1) {
 			if (errorObject) errorObject.add(errorCode, errorDesc, 400);
