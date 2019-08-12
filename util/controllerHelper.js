ba95ad4fd711c95a620854c9cdf5219fba1984ca
Abad Carrascosa <Alejandro_AC@hefame.es>
2019-08-12 12:46:32 +0200

    Arreglado bug en log de errores

--- util/controllerHelper.js
+++ util/controllerHelper.js
@@ -15,7 +15,7 @@ module.exports = {
 		} else { // Es una Excepcion standard
 			var error = new FedicomError('HTTP-500', 'Error interno del servidor - ' + req.txId, 500);
 			responseBody = error.send(res);
-			errorToLog =  ex.stack.split(/\r?\n/);
+			errorToLog =  ex.stack ? ex.stack.split(/\r?\n/) : (ex ? ex.toString() : 'null');
 		}
 
 		L.xe(req.txId, ['Se detectó un error al analizar la transmisión. Se transmite el error al cliente', errorToLog, responseBody]);
