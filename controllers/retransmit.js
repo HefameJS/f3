4d8df9cb7af73478fecdfbe2f3b3dd53e044abb7
Abad Carrascosa <Alejandro_AC@hefame.es>
2020-02-14 13:10:20 +0100

    Control de autenticacion en retrasnsmisiones

--- controllers/retransmit.js
+++ controllers/retransmit.js
@@ -1,15 +1,30 @@
 'use strict';
 const BASE = global.BASE;
-//const L = global.logger;
+const L = global.logger;
 //const C = global.config;
 //const K = global.constants;
 
 const retransmitirPedido = require(BASE + 'watchdog/retransmitirPedido').retransmitirPedido;
-
+const Tokens = require(BASE + 'util/tokens');
 
 exports.retransmitirPedido = function (req, res) {
 
 	var txId = req.params.txId;
+
+	req.token = Tokens.verifyJWT(req.token, txId);
+	if (req.token.meta.exception) {
+		L.xe(txId, ['El token de la transmisión no es válido. Se transmite el error al cliente', req.token], 'txToken');
+		req.token.meta.exception.send(res);
+		return;
+	}
+	if (!req.token.perms || !req.token.perms.includes('FED3_RETRANSMISION')) {
+		L.xw(txId, ['El usuario no tiene los permisos necesarios para retransmitir pedidos', req.token.perms])
+		var error = new FedicomError('AUTH-005', 'No tienes los permisos necesarios para realizar esta acción', 403);
+		error.send(res);
+		return;
+	}
+
+
 
 	var retransmissionOptions = {
 		force: (req.query.forzar === 'si') ? true : false,
