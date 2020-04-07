95e1fe3bdb29164fa31d515303e7edb9b7622e97
Alejandro AC <alejandro.abad@hefame.es>
2020-04-07 11:37:52 +0200

    No me gustan las promesas

--- interfaces/procesos/iRegistroProcesos.js
+++ interfaces/procesos/iRegistroProcesos.js
@@ -44,16 +44,16 @@ const detenerIntervaloRegistro = () => {
  * @param {function} callback function (err, false)
  */
 const soyMaestro = (tipoProceso, callback) => {
-	obtenerProcesoMaestro(tipoProceso, (err, watchdogs) => {
+	obtenerProcesoMaestro(tipoProceso, (err, procesos) => {
 		if (err) {
 			callback(err, false)
 			return;
 		}
 
-		let maestro = watchdogs[0]
+		let maestro = procesos[0]
 
 		if (!maestro || maestro.host === OS.hostname()) {
-			if (!maestro) L.i(['No hay procesos de watchdog compitiendo por ser maestros'], 'election')
+			if (!maestro) L.i(['No hay procesos compitiendo por ser maestros'], 'election')
 			if (!vecesGanadoMaestro[tipoProceso] || vecesGanadoMaestro[tipoProceso] < 2) {
 				if (!vecesGanadoMaestro[tipoProceso]) vecesGanadoMaestro[tipoProceso] = 0
 				vecesGanadoMaestro[tipoProceso]++
@@ -76,13 +76,13 @@ const soyMaestro = (tipoProceso, callback) => {
 			if (diff > 20000) {
 				L.w(['El maestro en ' + maestro.host + ' no ha dado señales de vida desde hace ' + diff / 1000 + ' segundos'], 'election')
 
-				for (let i = 1; i < watchdogs.length; i++) {
-					let candidato = watchdogs[i]
+				for (let i = 1; i < procesos.length; i++) {
+					let candidato = procesos[i]
 					if (!candidato) {break;}
 
 					if (candidato.host === OS.hostname()) {
 						L.i(['Yo soy el candidato con mayor prioridad, me postulo como próximo maestro'])
-						asumirMaestro()
+						asumirMaestro(tipoProceso)
 						callback(null, false)
 						return;
 					} else {
@@ -90,7 +90,7 @@ const soyMaestro = (tipoProceso, callback) => {
 						if (diff > 20000) {
 							L.w(['El candidato ' + candidato.host + 'no ha dado señales de vida desde hace ' + diff / 1000 + ' segundos. Probamos el siguiente'], 'election')
 						} else {
-							L.i(['Hay un candidato en ' + candidato.host + '  con mayor prioridad. Espero a que de señales de vida',], 'election')
+							L.i(['Hay un candidato en ' + candidato.host + ' con mayor prioridad. Espero a que de señales de vida',], 'election')
 							callback(null, false)
 							return;
 						}
@@ -144,8 +144,6 @@ module.exports = {
 	soyMaestro,
 	consultaProcesos
 }
-
-
 
 
 
@@ -213,14 +211,16 @@ const obtenerProcesoMaestro = (tipoProceso, callback) => {
 
 	let control = Imongo.coleccionControl();
 	if (control) {
-		control.find(filtro).sort({ priority: -1 }).toArray()
-			.then(res => {
-				callback(null, res)
-			})
-			.catch(err => {
-				L.e(['Error al obtener el proceso maestro', err], 'election');
+		control.find(filtro).sort({ priority: -1 }).toArray( (err, res) => {
+			if (err) {
+				L.e(['Error al obtener el proceso maestro', filtro, err], 'election');
 				callback(err, null)
-			})
+				return;
+			}
+
+			callback(null, res);
+
+		})
 	} else {
 		L.e(['Error al obtener el proceso maestro. No conectado a MDB'], 'election');
 		callback({ error: 'No conectado a MDB' }, null)
@@ -232,24 +232,25 @@ const obtenerProcesoMaestro = (tipoProceso, callback) => {
  * Hacer que el proceso lance una petición a la tabla de control
  * para hacerse maestro. 
  */
-const asumirMaestro = () => {
+const asumirMaestro = (tipoProceso) => {
 
 	let filtro = {
-		type: K.PROCESS_TYPES.WATCHDOG,
+		type: tipoProceso,
 		host: { $ne: OS.hostname() }
 	}
 
 	let control = Imongo.coleccionControl();
 	if (control) {
-		control.updateMany(filtro, { $set: { priority: -1, maestro: 0 } })
-			.then(res => {
-				L.e(['Lanzada petición a la red para obtener el rol de watchdog maestro'], 'election');
-			})
-			.catch(err => {
-				L.e(['Error al asumir el rol de watchdog maestro', err], 'election');
-			})
+		control.updateMany(filtro, { $set: { priority: -1, maestro: 0 } }, (err, res) => {
+			if (err) {
+				L.e(['Error al asumir el rol de maestro', err], 'election');
+				return;
+			}
+			L.i(['Lanzada petición a la red para obtener el rol de maestro', filtro], 'election');
+		})
+
 	} else {
-		L.e(['Error al asumir el rol de watchdog maestro. No conectado a MDB'], 'election');
+		L.e(['Error al asumir el rol de maestro. No conectado a MDB'], 'election');
 	}
 }
 
