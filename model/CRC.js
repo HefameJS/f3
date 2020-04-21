a9e9ccd98f3d7ba9fd81e182bc00fb1ab3034de9
Alejandro AC <alejandro.abad@hefame.es>
2020-04-21 10:53:47 +0200

    CRC.crearParaLineaDevolucion

--- model/CRC.js
+++ model/CRC.js
@@ -7,10 +7,21 @@
 const crypto = require('crypto');
 
 const crear = (codigoCliente, numeroPedidoOrigen) => {
+
 	let hash = crypto.createHash('sha1');
-	return hash.update(codigoCliente + numeroPedidoOrigen).digest('hex').substring(1, 25).toUpperCase();
+	return hash.update(codigoCliente + numeroPedidoOrigen)
+		.digest('hex').substring(1, 25).toUpperCase();
+
+}
+
+const crearParaLineaDevolucion = (codigoMotivo, numeroAlbaran, fechaAlbaran, codigoArticulo, cantidad, lote, fechaCaducidad, valeEstupefaciente) => {
+
+	let hash = crypto.createHash('sha1');
+	return hash.update(codigoMotivo + numeroAlbaran + fechaAlbaran + codigoArticulo + cantidad + lote + fechaCaducidad + valeEstupefaciente)
+		.digest('hex').substring(1, 25).toUpperCase();
 }
 
 module.exports = {
-	crear: crear
+	crear,
+	crearParaLineaDevolucion
 }
