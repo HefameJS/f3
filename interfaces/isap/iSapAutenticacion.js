e53ddb74eb4d691eeb06a8d84486ac192de1984f
Alejandro AC <alejandro.abad@hefame.es>
2020-04-29 17:23:59 +0200

    Fix: C -> K

--- interfaces/isap/iSapAutenticacion.js
+++ interfaces/isap/iSapAutenticacion.js
@@ -1,7 +1,7 @@
 'use strict';
-const C = global.config;
+//const C = global.config;
 const L = global.logger;
-//const K = global.constants;
+const K = global.constants;
 
 // Externas
 const request = require('request');
