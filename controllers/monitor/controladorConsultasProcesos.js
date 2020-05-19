1e0763779d94227bd24668478ef441b4a5312379
Alejandro AC <alejandro.abad@hefame.es>
2020-05-19 16:11:00 +0200

    Modernizada consulta de procesos

--- controllers/monitor/controladorConsultasProcesos.js
+++ controllers/monitor/controladorConsultasProcesos.js
@@ -3,40 +3,18 @@
 const L = global.logger;
 //const K = global.constants;
 
+// Externas
+const OS = require('os');
+
 // Interfaces
 const iTokens = require('util/tokens');
 const iRegistroProcesos = require('interfaces/procesos/iRegistroProcesos')
 
-// GET /status/proc
-const consultaProcesos = (req, res) => {
-	let txId = req.txId;
-
-	L.xi(txId, ['Consulta de procesos']);
-
-	// Verificación del token del usuario
-	let estadoToken = iTokens.verificaPermisos(req, res);
-	if (!estadoToken.ok) return;
+// Modelos
+const ErrorFedicom = require('model/ModeloErrorFedicom');
 
 
-	let tipoProceso = req.query.type ? req.query.type : null;
-	let host = req.query.host ? req.query.host : null;
-
-
-	iRegistroProcesos.consultaProcesos(tipoProceso, host, (errorRegistroProcesos, procesos) => {
-		if (errorRegistroProcesos) {
-			L.xe(txId, ['Error al obtener la lista de procesos', errorRegistroProcesos]);
-			res.status(500).json({ ok: false, error: (errorRegistroProcesos.error || errorRegistroProcesos.message) });
-			return;
-		}
-		L.xi(txId, ['Obtenida lista de procesos']);
-		res.status(200).json({ ok: true, data: procesos });
-		return;
-
-	} )
-}
-
-
-// GET /procesos ? [tipo=<tipo-proceso>] & [servidor=<host-proceso>]
+// GET /procesos ? [servidor=<host-proceso>] & [tipo=<tipo-proceso>]
 const listadoProcesos = (req, res) => {
 
 	let txId = req.txId;
@@ -50,14 +28,18 @@ const listadoProcesos = (req, res) => {
 	let tipoProceso = req.query.tipo ? req.query.tipo : null;
 	let servidor = req.query.servidor ? req.query.servidor : null;
 
+	if (servidor === 'local') {
+		servidor = OS.hostname();
+	}
+
 	iRegistroProcesos.consultaProcesos(tipoProceso, servidor, (errorRegistroProcesos, procesos) => {
 		if (errorRegistroProcesos) {
 			L.xe(txId, ['Error al obtener la lista de procesos', errorRegistroProcesos]);
-			res.status(500).json({ ok: false, error: (errorRegistroProcesos.error || errorRegistroProcesos.message) });
+			ErrorFedicom.generarYEnviarErrorMonitor(res, 'Error al obtener la lista de procesos');
 			return;
 		}
 		L.xi(txId, ['Obtenida lista de procesos']);
-		res.status(200).json({ ok: true, data: procesos });
+		res.status(200).json(procesos);
 		return;
 
 	})
@@ -68,6 +50,5 @@ const listadoProcesos = (req, res) => {
 
 
 module.exports = {
-	listadoProcesos,
-	consultaProcesos // GET /status/proc
+	listadoProcesos
 }
