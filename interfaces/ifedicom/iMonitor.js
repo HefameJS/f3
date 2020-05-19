b6b3e4ea30a57f28ab159c217d99c95aa15730a9
Alejandro AC <alejandro.abad@hefame.es>
2020-05-19 13:04:20 +0200

    Simplificada interfaz de llamadas
    Ahora se enera un único token Interfedicom que se utiliza hasta caducas

--- interfaces/ifedicom/iMonitor.js
+++ interfaces/ifedicom/iMonitor.js
@@ -11,8 +11,8 @@ const request = require('request');
 const iTokens = require('util/tokens');
 const iProcesos = require('interfaces/procesos/iRegistroProcesos');
 
-
-
+// 
+let tokenIntermonitor = null;
 
 /**
  * Condensa en un solo objeto la respuesta dada por un monitor remoto.
@@ -26,15 +26,38 @@ const _ampliaRespuestaMonitor = (repuestaMonitor, cuerpoMonitor) => {
 	return repuestaMonitor;
 }
 
+const _obtenerTokenIntermonitor = () => {
+	
+		if (!tokenIntermonitor) {
+			tokenIntermonitor = iTokens.generarTokenInterFedicom();
+			return tokenIntermonitor;
+		}
 
-const realizarLlamadaInterna = (destino, ruta, callback) => {
+		let estadoToken = iTokens.verificarToken(tokenIntermonitor);
+		if (!estadoToken.meta.ok) {
+			tokenIntermonitor = iTokens.generarTokenInterFedicom();
+			return tokenIntermonitor;
+		} else {
+			return tokenIntermonitor;
+		}
+
+}
+_obtenerTokenIntermonitor();
+
+/**
+ * Realiza la llamada intermonitor al destino indicado.
+ * @param {*} destino 
+ * @param {*} ruta 
+ * @param {*} callback 
+ */
+const _realizarLlamadaAMonitor = (destino, ruta, callback) => {
 
 	let parametrosLlamada = {
 		followAllRedirects: true,
 		uri: 'http://' + destino + ':5001' + ruta,
 		json: true,
 		headers: {
-			Authorization: 'Bearer ' + iTokens.generarTokenInterFedicom()
+			Authorization: 'Bearer ' + _obtenerTokenIntermonitor()
 		}
 	}
 
@@ -49,8 +72,6 @@ const realizarLlamadaInterna = (destino, ruta, callback) => {
 			callback(errorLlamada, repuestaMonitor)
 			return;
 		}
-
-		repuestaMonitor = _ampliaRespuestaMonitor(repuestaMonitor, cuerpoMonitor);
 
 		if (repuestaMonitor.error) {
 			L.e(['El monitor remoto retornó un mensaje de error', destino, repuestaMonitor.statusCode], 'iMonitor');
@@ -68,64 +89,105 @@ const realizarLlamadaInterna = (destino, ruta, callback) => {
 }
 
 
-const realizarLlamadaMultiple = (ruta, callback) => {
+/**
+ * Realiza la llamada intermonitor a la lista de destinos pasada.
+ * IMPRESCINDIBLE QUE LA LISTA DE DESTINOS SEA UN ARRAY Y CONTENGA AL MENOS UN DESTINO.
+ * @param {*} destinos Array con los nombres de los destinos.
+ * @param {*} ruta 
+ * @param {*} callback 
+ */
+const _llamadaAMultiplesDestinos = (destinos, ruta, callback) => {
 
-	iProcesos.consultaProcesos(K.PROCESS_TYPES.MONITOR, null, (errorProcesos, procesos) => {
-		if (errorProcesos) {
-			L.e(['No se pudieron obtener los procesos de tipo monitor', errorProcesos])
-			callback(errorProcesos, null);
-			return;
+
+	L.d(['Se procede a realizar la llamada a multiples destinos', destinos, ruta]);
+
+	if (!destinos || destinos.length === 0) {
+		L.e(['No está permitido llamar a _llamadaAMultiplesDestinos() sin especificar ningún destino !', destinos]);
+		callback(new Error('No se ha especificado ningún destino'), null);
+		return;
+	}
+
+	let mutex = lock.createMutex();
+	let respuestasAlglomeradas = {};
+	let respuestasPendientes = destinos.length;
+
+	let funcionAlglomeradora = (destino, error, respuesta) => {
+
+		respuestasAlglomeradas[destino] = {}
+
+		if (error) {
+			respuestasAlglomeradas[destino].ok = false;
+			if (respuesta.body) respuestasAlglomeradas[destino].error = respuesta.body;
+			else respuestasAlglomeradas[destino].error = error;
+		} else {
+			respuestasAlglomeradas[destino].ok = !respuesta.error;
+			if (respuesta.body) respuestasAlglomeradas[destino].respuesta = respuesta.body;
 		}
 
-		let destinos = procesos.map(proceso => proceso.host);
-		L.d(['Se procede a realizar la llamada multiple', destinos, ruta]);
+
+		mutex.lock(() => {
+			respuestasPendientes--;
+			if (respuestasPendientes === 0) {
+				callback(null, respuestasAlglomeradas);
+			}
+			mutex.unlock();
+		});
+	}
+
+	destinos.forEach(destino => {
+		_realizarLlamadaAMonitor(destino, ruta, (errorLlamada, respuestaLlamada) => {
+			funcionAlglomeradora(destino, errorLlamada, respuestaLlamada);
+		})
+	});
+}
 
 
-		let mutex = lock.createMutex();
-		let respuestasAlglomeradas = {};
-		let respuestasPendientes = destinos.length;
+/**
+ * Realiza la llamada intermonitor a la lista de destinos pasada.
+ * Puede pasarse un único destino como un string, o una lista de destinos en un array de strings
+ * Si no se indica ningún destino, se busca en el registro de procesos todos los procesos de tipo monitor.
+ * @param {*} destinos 
+ * @param {*} ruta 
+ * @param {*} callback 
+ */
+const realizarLlamadaMultiple = (destinos, ruta, callback) => {
 
-		let funcionAlglomeradora = (destino, error, respuesta) => {
 
-			respuestasAlglomeradas[destino] = {}
+	if (destinos && destinos.forEach && destinos.length > 0) {
+		// Destinos es un array con al menos una posicion.
+		_llamadaAMultiplesDestinos(destinos, ruta, callback);
+		return;
+	} else if (destinos) {
+		// Destinos no es vacío, pero no es un array,
+		_llamadaAMultiplesDestinos([destinos], ruta, callback);
+		return;
+	} else {
 
-			if (error) {
-				respuestasAlglomeradas[destino].ok = false;
-				
-				if (respuesta.body) respuestasAlglomeradas[destino].error = respuesta.body;
-				else respuestasAlglomeradas[destino].error = error;
+		iProcesos.consultaProcesos(K.PROCESS_TYPES.MONITOR, null, (errorProcesos, procesos) => {
+			if (errorProcesos) {
+				L.e(['No se pudieron obtener los procesos de tipo monitor', errorProcesos])
+				callback(errorProcesos, null);
+				return;
+			}
+
+			let destinos = procesos.map(proceso => proceso.host);
+			if (destinos && destinos.forEach && destinos.length > 0) {
+				_llamadaAMultiplesDestinos(destinos, ruta, callback);
+				return;
 			} else {
-				respuestasAlglomeradas[destino].ok = respuesta.body.ok;
-				if (respuesta.body.data) respuestasAlglomeradas[destino].data = respuesta.body.data;
-				if (respuesta.body.error) respuestasAlglomeradas[destino].data = respuesta.body.error;
+				L.e(['No se obtuvieron procesos de tipo monitor'])
+				callback(new Error('No se obtuvieron procesos de tipo monitor'), null);
+				return;
 			}
 
 
-			mutex.lock(() => {
-				respuestasPendientes--;
-				if (respuestasPendientes === 0) {
-					callback(null, respuestasAlglomeradas);
-				}
-				mutex.unlock();
-			});
-		}
-
-		destinos.forEach(destino => {
-			realizarLlamadaInterna(destino, ruta, (errorLlamada, respuestaLlamada) => {
-				funcionAlglomeradora(destino, errorLlamada, respuestaLlamada);
-			})
 		});
-
-
-
-	})
-
-
+	}
 
 
 }
 
+
 module.exports = {
-	realizarLlamadaInterna,
 	realizarLlamadaMultiple
 }
