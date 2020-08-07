e20694e1856ccff7c43cd32b3142bccc7934868e
Alejandro AC <alejandro.abad@hefame.es>
2020-08-07 11:11:35 +0200

    iEventosComun: Auntenticación

--- interfaces/eventos/iEventosAutenticacion.js
+++ interfaces/eventos/iEventosAutenticacion.js
@@ -7,6 +7,7 @@ const K = global.constants;
 const clone = require('clone');
 
 // Interfaces
+const iEventosComun = require('./iEventosComun');
 const iMongo = require('interfaces/imongo/iMongo');
 const iFlags = require('interfaces/iFlags');
 
@@ -15,65 +16,25 @@ module.exports.inicioAutenticacion = (req) => {
 
 	let txId = req.txId;
 
-	let transaccion = {
-		$setOnInsert: {
-			_id: txId,
-			createdAt: new Date()
-		},
-		$max: {
-			modifiedAt: new Date(),
-			status: K.TX_STATUS.RECEPCIONADO
-		},
-		$set: {
-			authenticatingUser: req.identificarUsuarioAutenticado(),
-			iid: global.instanceID,
-			type: K.TX_TYPES.AUTENTICACION,
-			clientRequest: {
-				ip: req.originIp,
-				protocol: req.protocol,
-				method: req.method,
-				url: req.originalUrl,
-				route: req.route.path,
-				headers: req.headers,
-				body: clone(req.body) // Clone necesario para poder eliminar la password mas adelante
-			}
-		}
-	}
+	let transaccion = iEventosComun.generarEventoDeApertura(req, K.TX_TYPES.AUTENTICACION, K.TX_STATUS.RECEPCIONADO);
 
 	// Ocultamos la contraseña del usuario en los logs
-	if (transaccion['$set'].clientRequest.body.password) 
-		transaccion['$set'].clientRequest.body.password = '******';
+	if (transaccion['$set'].clientRequest.body.password) {
+		transaccion['$set'].clientRequest.body = clone(transaccion['$set'].clientRequest.body);
+		transaccion['$set'].clientRequest.body.password = '*******'
+	}
 
 	L.xi(txId, ['Emitiendo COMMIT para evento AuthRequest'], 'txCommit');
 	iMongo.transaccion.grabarEnMemoria(transaccion);
 }
 
-module.exports.finAutenticacion = (res, cuerpoRespuesta, status) => {
+module.exports.finAutenticacion = (res, cuerpoRespuesta, estado) => {
 
 	let txId = res.txId;
 
-	let transaccion = {
-		$setOnInsert: {
-			_id: txId,
-			createdAt: new Date()
-		},
-		$max: {
-			modifiedAt: new Date(),
-			status: status,
-		},
-		$set: {
-			clientResponse: {
-				timestamp: new Date(),
-				status: res.statusCode,
-				headers: res.getHeaders(),
-				body: cuerpoRespuesta
-			}
-		}
-	}
-
+	let transaccion = iEventosComun.generarEventoDeCierre(res, cuerpoRespuesta, estado);
 	iFlags.finaliza(txId, transaccion);
 
-	
 	L.xi(txId, ['Emitiendo COMMIT para evento AuthResponse'], 'txCommit');
 	iMongo.transaccion.grabar(transaccion);
 }
