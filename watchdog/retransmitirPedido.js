65bc168272572a7a8e76d1feeaaa02dc22423056
Alejandro Abad <alejandro.abad@hefame.es>
2021-04-15 12:34:53 +0200

    importacion de iFlags

--- watchdog/retransmitirPedido.js
+++ watchdog/retransmitirPedido.js
@@ -8,6 +8,7 @@ const M = global.mongodb;
 const iMongo = require('interfaces/imongo/iMongo');
 const iSap = require('interfaces/isap/iSap');
 const iEventos = require('interfaces/eventos/iEventos');
+const iFlags = require('interfaces/iFlags');
 
 // Modelos
 const ErrorFedicom = require('modelos/ErrorFedicom');
