9720c3f05f871c352957048d23c59b82edabaa1c
Alejandro Abad <alejandro.abad@hefame.es>
2021-05-12 15:11:08 +0200

    Inicio de jerarquia de flags

--- interfaces/iflags/iFlags.js
+++ interfaces/iflags/iFlags.js
@@ -1,56 +1,17 @@
 'use strict';
-const C = global.config;
-const L = global.logger;
-const K = global.constants;
-const M = global.mongodb;
+//const C = global.config;
+//const L = global.logger;
+//const K = global.constants;
+//const M = global.mongodb;
 
 // Externos
-const memCache = require('memory-cache');
-const cacheFlags = new memCache.Cache();
-cacheFlags.countStats(false);
 
-
-const set = (txId, flagName, value = true ) => {
-
-    if (!txId) { L.e('No se ha especificado ID de transmisión', 'iFlags'); return; }
-	if (!flagName) { L.e('No se ha especificado nombre del flag', 'iFlags'); return; }
-
-	L.xt(txId, ['Estableciendo flag', flagName, value, txId], 'iFlags');
-
-    txId = new M.ObjectID(txId);
-    let flags = cacheFlags.get(txId) || {};
-    flags[flagName] = value;
-
-    cacheFlags.put(txId, flags);
-}
-
-const get = (txId) => {
-    let flags = cacheFlags.get(new M.ObjectID(txId));
-    return flags || {};
-}
-
-const finaliza = (txId, mdbQuery) => {
-    let flags = get(txId);
-    flags[C.flags.VERSION] = K.VERSION.TRANSMISION;
-
-    if (!mdbQuery.$set) mdbQuery.$set = {};
-
-	L.xd(txId, ['Finalizando flags', flags, txId], 'iFlags');
-    
-    for (let flag in flags) {
-        mdbQuery.$set['flags.' + flag] = flags[flag];
-    }
-	del(txId);
-}
-
-const del = (txId) => {
-	L.xt(txId, ['Borrando flags', txId], 'iFlags');
-    cacheFlags.del(new M.ObjectID(txId));
-}
+const iFlagsComun = require('./iFlagsComun');
 
 module.exports = {
-    set,
-    get,
-    del,
-    finaliza
+	set: iFlagsComun.set,
+	get: iFlagsComun.get,
+	del: iFlagsComun.del,
+	finaliza: iFlagsComun.finaliza,
+	transmision: require('./iFlagsTransmision')
 };
