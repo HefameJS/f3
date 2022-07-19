5d63f2fb3e0f6210ca69bdcae12eafb43cd60d10
Alejandro <alejandro.abad@hefame.es>
2022-07-19 13:00:58 +0200

    Ordenacion de lineas en entrada de pedido
    Para evitar que el cliente, al desordenar líneas, provoque que no se detecte el pedido duplicado.

--- modelos/pedido/ModeloPedidoCliente.js
+++ modelos/pedido/ModeloPedidoCliente.js
@@ -155,6 +155,8 @@ class PedidoCliente {
 		this.lineas = [];
 		let ordenes = [];
 
+		lineas.sort((a, b) => a.codigoArticulo < b.codigoArticulo ? -1 : 1);
+
 		lineas.forEach((linea, i) => {
 			let lineaPedido = new LineaPedidoCliente(linea, txId, i);
 
