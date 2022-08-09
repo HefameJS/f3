31448c4646814759df177789352434c41e71de4c
Alejandro Abad Carrascosa <alejandro.abad@hefame.es>
2022-08-09 22:43:48 +0200

    Eliminada traza redundante

--- global/websocket/clienteWebsocketInterior.js
+++ global/websocket/clienteWebsocketInterior.js
@@ -55,7 +55,7 @@ class ClienteWebsocketInterior {
 	}
 
 	enviarMensaje(mensaje, envioDesdeBuffer) {
-		this.#log.trace('Enviando mensaje al recolector', mensaje)
+		// this.#log.trace('Enviando mensaje al recolector', mensaje)
 
 		if (!this.#websocket) {
 			this.#log.trace('No existe la conexión al recolector')
