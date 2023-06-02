de5c3081b1f54e201f63b3bb0b4c9596c8a2a511
Alejandro <alejandro.abad@hefame.es>
2023-06-02 11:26:38 +0200

    Ahora se devuelve el numero de albarán original

--- controladores/controladorAlbaranes.js
+++ controladores/controladorAlbaranes.js
@@ -62,7 +62,7 @@ const _consultaAlbaranPDF = async function (req, res, numAlbaran) {
 
 }
 
-const _consultaAlbaranJSON = async function (req, res, numAlbaran, devolverComoArray) {
+const _consultaAlbaranJSON = async function (req, res, numAlbaran, devolverComoArray, numAlbaranOriginal) {
 	let txId = req.txId;
 
 	try {
@@ -72,6 +72,7 @@ const _consultaAlbaranJSON = async function (req, res, numAlbaran, devolverComoA
 		// Comprobamos que SAP haya devuelto al menos un objeto con el campo de posiciones
 		if (cuerpoSap?.t_pos) {
 			let datosAlbaran = new Albaran(cuerpoSap);
+			datosAlbaran.numeroAlbaran = numAlbaranOriginal;
 			if (devolverComoArray) datosAlbaran = [datosAlbaran];
 			res.status(200).json(datosAlbaran);
 			iEventos.consultas.consultaAlbaran(req, res, { json: numAlbaran }, K.TX_STATUS.OK, numAlbaran, 'JSON');
@@ -136,7 +137,7 @@ const consultaAlbaran = async function (req, res) {
 
 	switch (formatoAlbaran) {
 		case 'JSON':
-			return _consultaAlbaranJSON(req, res, numAlbaranSaneado);
+			return _consultaAlbaranJSON(req, res, numAlbaranSaneado, false, numAlbaran);
 		case 'PDF':
 			return _consultaAlbaranPDF(req, res, numAlbaranSaneado);
 		default:
